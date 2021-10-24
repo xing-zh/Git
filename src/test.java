43626afff77634e29ec5356dc7e7932a2e10bc31
Zhu zhenxin <1836770152@qq.com>
2021-10-24 17:24:13 +0800

    默认变更列表

--- src/test.java
+++ src/test.java
@@ -2,6 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("aaaaaa");
         System.out.println("bbbbbb");
-
+        int[][] arr=new int[][]{{1,2,3,},{4,5},{6,7,8}};
+        System.out.println("ffffffff");
     }
     }
